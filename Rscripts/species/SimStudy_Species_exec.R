44bf9d0b51ad29ddd90285e744c1ffdac261d318
Lucia Paci <lucia.paci@localhost.localdomain>
2025-05-29 17:12:04 +0200

    fit species zipfs and NegBin from VM catto

--- Rscripts/species/SimStudy_Species_exec.R
+++ Rscripts/species/SimStudy_Species_exec.R
@@ -15,7 +15,7 @@ source("../../R/Rfunctions.R")
 
 # Common parameters -------------------------------------------------------
 alfa = 0.05
-Bor  = 3 # nel suo codice, painsky ripete 10k volte, noi 5k?
+Bor  = 100 # nel suo codice, painsky ripete 10k volte, noi 5k?
 Bnoi = Bor 
 n = 1000
 Rmax = 100; RmaxFD = 50
@@ -27,7 +27,7 @@ seed = 42*idx
 set.seed(seed)
 
 # Zipfs -------------------------------------------------------------------
-run_zipfs = TRUE
+run_zipfs = FALSE
 
 if(run_zipfs){
   s = 1.01
@@ -37,7 +37,7 @@ if(run_zipfs){
   
   pb <- progress_bar$new(total = Nexp)
   for(ii in 1:Nexp){
-    cat("\n #Exp. = ",ii," \n")
+    cat("\n Zipfs, #Exp. = ",ii," \n")
     M = Mgrid[ii]
     # Gen. true distribution
     ptrue = sim_zipfs(M = M, s = s)
@@ -102,3 +102,79 @@ if(run_zipfs){
   
 }
 
+
+# Negative binomial -------------------------------------------------------------------
+l = 1; r = 0.003
+run_nb = TRUE
+
+if(run_nb){
+  
+  pain_mat = matrix(ub_pain(n = n, Rmax = Rmax, alfa = alfa), Nexp, Bnoi)
+  lub_Mrk_mat <- lub_FD_mat <- oracle_mat <- matrix(NA,Nexp,Bnoi)
+  
+  pb <- progress_bar$new(total = Nexp)
+  for(ii in 1:Nexp){
+    cat("\n Neg. Bin. #Exp. = ",ii," \n")
+    M = Mgrid[ii]
+    # Gen. true distribution
+    ptrue = sim_negbin(M = M, l = l, r = r)
+    # plot(ptrue)
+    
+    # Oracle
+    Mmax = rep(NA,Bor)
+    for(b in 1:Bor){
+      # Gen. data
+      data = sample(1:M, size = n, replace = TRUE, prob = ptrue)
+      n_i = tabulate(data, nbins = M)
+      idx_obs = which(n_i > 0)
+      Kn = length(idx_obs)
+      data_obs = n_i[idx_obs]
+      if(Kn == M){
+        Mmax[b] = 0
+      }else{
+        idx_unobs = which(n_i == 0)
+        Mmax[b] = max(ptrue[idx_unobs])
+      }
+      oracle_mat[ii,b] = Mmax[b]
+      
+      # oracle[ii] = quantile(Mmax, probs = 1-alfa)
+      
+      # Markov - Cantelli - FD
+      # Param. estimation (PYP)
+      start_params <- c(alpha = 0.1, theta = 1)
+      fit <- optim(par = start_params, fn = llik_pyp, 
+                   method = "L-BFGS-B",
+                   lower = c(0, -1), upper = c(1-1e-10, Inf),
+                   n = n, Kn = Kn, data_obs = data_obs) 
+      alpha_mle = fit$par[1]
+      theta_mle = fit$par[2]
+      
+      # Upper bound (PYP)
+      lub_Mrk_mat[ii,b] = compute_log_UBMarkov( Rmax, alpha_mle, theta_mle, 
+                                                Kn, n, alfa )
+      
+      # Param. estimation (FD)
+      start_params <- c(gamma = 0.1, Lambda = Kn)
+      fit <- optim(par = start_params, fn = llik_FD,
+                   n = n, Kn = Kn, data_obs = data_obs, M_max = M_max,
+                   method = "L-BFGS-B",
+                   lower = c(1e-5, 1e-5), upper = c(Inf, Inf)) 
+      gamma_mle = fit$par[1]
+      Lambda_mle = fit$par[2]
+      
+      # Upper bound (FD)
+      lub_FD_mat[ii,b] = compute_log_UBMarkov_FD( RmaxFD, gamma_mle, Lambda_mle, Kn, n, alfa, M_max )
+    }
+    pb$tick()
+  }
+  
+  save_res = list("lub_Mrk_mat" = lub_Mrk_mat, 
+                  "lub_FD_mat"  = lub_FD_mat,
+                  "pain_mat"    = pain_mat,
+                  "oracle_mat" = oracle_mat)
+  
+  
+  file_name = paste0("../save/SimStudySpecies_NegBib_",idx,".Rdat")
+  save(save_res, file = file_name) 
+  
+}
--- Rscripts/species/SimStudy_Species_plot.R
+++ Rscripts/species/SimStudy_Species_plot.R
@@ -15,8 +15,8 @@ source("../../R/Rfunctions.R")
 
 # Common parameters -------------------------------------------------------
 alfa = 0.05
-Bor  = 3 # nel suo codice, painsky ripete 10k volte, noi 5k?
-Bnoi = Bor 
+# Bor  = 3 # nel suo codice, painsky ripete 10k volte, noi 5k?
+# Bnoi = Bor 
 n = 1000
 Rmax = 100; RmaxFD = 50
 Mgrid = seq(10,1010,by = 50)
@@ -25,11 +25,14 @@ M_max = 500
 
 
 # Zipfs: load data (from different repetitions) -------------------------------------------------------------------
-# stop("Data have all been collected in SimStudyFeatures_zipfs_all.Rdat ")
+stop("Data have all been collected in SimStudyFeatures_zipfs_all.Rdat ")
 s = 1.01
 Nsim = 10
+sim_idx = 11:30
+sim_idx = sim_idx[-11]
+
 pain_mat <- lub_Mrk_mat <- lub_FD_mat <- oracle_mat <- c()
-for(ii in 1:Nsim){
+for(ii in sim_idx){
   file_name = paste0("../save/SimStudySpecies_zipfs_",ii,".Rdat")  
   load(file_name)
   pain_mat = cbind(pain_mat, save_res$pain_mat)
@@ -38,13 +41,12 @@ for(ii in 1:Nsim){
   oracle_mat = cbind(oracle_mat,save_res$oracle_mat)
 }
 
-# Save collected dataset
-# save_res = list("lub_PP3_mat" = lub_PP3_mat,
-#                 "lub_MixPois_mat" = lub_MixPois_mat,
-#                 "lub_MixBin_mat"  = lub_MixBin_mat,
-#                 "ub_Freq_mat"  = ub_Freq_mat,
+# # Save collected dataset
+# save_res = list("lub_Mrk_mat" = lub_Mrk_mat,
+#                 "lub_FD_mat" = lub_FD_mat,
+#                 "pain_mat"  = pain_mat,
 #                 "oracle_mat"  = oracle_mat)
-# file_name = paste0("../save/SimStudyFeatures_zipfs_all.Rdat")
+# file_name = paste0("../save/SimStudySpecies_zipfs_all.dat")
 # save(save_res, file = file_name)
 
 # Zipfs: load data (all at once) -------------------------------------------------------------------
@@ -52,13 +54,11 @@ for(ii in 1:Nsim){
 s = 1.01
 var_gamma = 10
 var_nb    = 10
-Nrep = 5
 
-load("../save/SimStudyFeatures_zipfs_all.Rdat")
-lub_PP3_mat = save_res$lub_PP3_mat
-lub_MixPois_mat = save_res$lub_MixPois_mat
-ub_Freq_mat = save_res$ub_Freq_mat
-lub_MixBin_mat = save_res$lub_MixBin_mat
+load("../save/SimStudySpecies_zipfs_all.dat")
+lub_Mrk_mat = save_res$lub_Mrk_mat
+lub_FD_mat = save_res$lub_FD_mat
+pain_mat = save_res$pain_mat
 oracle_mat = save_res$oracle_mat
 
 # Compute final summary ---------------------------------------------------
@@ -66,16 +66,17 @@ oracle_mat = save_res$oracle_mat
 ub_PYP = exp(apply(lub_Mrk_mat, 1, quantile, probs = c(0.025,0.5,0.975)))
 ub_FD = exp(apply(lub_FD_mat, 1, quantile, probs = c(0.025,0.5,0.975)))
 ub_pain = apply(pain_mat, 1, quantile, probs = c(0.025,0.5,0.975))
-ub_oracle = apply(oracle_mat, 1, quantile, probs = c(0.025,0.5,0.975))
+ub_oracle = apply(oracle_mat, 1, quantile, probs = 1-alfa)
+
 
 # Zipfs: Plot curves -------------------------------------------------------------------
-ymax = (11/10) * (max(ub_oracle[2,],ub_PYP[2,],ub_FD[2,],ub_pain[2,]))
+ymax = (11/10) * (max(ub_oracle,ub_PYP[2,],ub_FD[2,],ub_pain[2,]))
 ymin = 0
 ylabs = round(seq(0,ymax,length.out = 5),3)
 
 
-save_img = FALSE
-img_name = paste0("../img/SSSpecies.pdf")
+save_img = TRUE
+img_name = paste0("../img/SSSpecies_zipfs.pdf")
 
 if(save_img)
   pdf(img_name)
@@ -89,7 +90,7 @@ grid(lty = 1,lwd = 1, col = "gray90" )
 axis(side = 2, at = ylabs, 
      labels = ylabs, las = 1, 
      cex.axis = 1 )
-points( x = Mgrid, y = ub_oracle[2,], 
+points( x = Mgrid, y = ub_oracle, 
         type = "b", lwd = 1, pch = 16, lty = 2,
         col = "black" ) 
 points( x = Mgrid, y = ub_PYP[2,], 
@@ -120,4 +121,109 @@ coverages = rbind(PYP_cov,FDP_cov,Freq_cov)
 coverages = as.data.frame(coverages)
 names(coverages) = paste0("M=",as.character(Mgrid))
 coverages
+
+file_name = paste0("../save/SimStudySpecies_coverages_zipfs_all.Rdat")
+save(coverages, file = file_name)
+
+
+
+
+
+
+# Neg. Bin.: load data (from different repetitions) -------------------------------------------------------------------
+stop("Data have all been collected in SimStudyFeatures_NegBin_all.Rdat ")
+l = 1; r = 0.003
+Nsim = 10
+sim_idx = 1:20
+# sim_idx = sim_idx[-11]
+
+pain_mat <- lub_Mrk_mat <- lub_FD_mat <- oracle_mat <- c()
+for(ii in sim_idx){
+  file_name = paste0("../save/SimStudySpecies_NegBib_",ii,".Rdat")  
+  load(file_name)
+  pain_mat = cbind(pain_mat, save_res$pain_mat)
+  lub_Mrk_mat = cbind(lub_Mrk_mat, save_res$lub_Mrk_mat)
+  lub_FD_mat = cbind(lub_FD_mat,save_res$lub_FD_mat)
+  oracle_mat = cbind(oracle_mat,save_res$oracle_mat)
+}
+
+# Save collected dataset
+# save_res = list("lub_Mrk_mat" = lub_Mrk_mat,
+#                 "lub_FD_mat" = lub_FD_mat,
+#                 "pain_mat"  = pain_mat,
+#                 "oracle_mat"  = oracle_mat)
+# file_name = paste0("../save/SimStudySpecies_NegBin_all.dat")
+# save(save_res, file = file_name)
+
+# NegBin: load data (all at once) -------------------------------------------------------------------
+
+load("../save/SimStudySpecies_NegBin_all.Rdat")
+lub_Mrk_mat = save_res$lub_Mrk_mat
+lub_FD_mat = save_res$lub_FD_mat
+pain_mat = save_res$pain_mat
+oracle_mat = save_res$oracle_mat
+
+# Compute final summary ---------------------------------------------------
+
+ub_PYP = exp(apply(lub_Mrk_mat, 1, quantile, probs = c(0.025,0.5,0.975)))
+ub_FD = exp(apply(lub_FD_mat, 1, quantile, probs = c(0.025,0.5,0.975)))
+ub_pain = apply(pain_mat, 1, quantile, probs = c(0.025,0.5,0.975))
+ub_oracle = apply(oracle_mat, 1, quantile, probs = 1-alfa)
+
+
+# NegBin: Plot curves -------------------------------------------------------------------
+ymax = (11/10) * (max(ub_oracle,ub_PYP[2,],ub_FD[2,],ub_pain[2,]))
+ymin = 0
+ylabs = round(seq(0,ymax,length.out = 5),3)
+
+
+save_img = TRUE
+img_name = paste0("../img/SSSpecies_NegBin.pdf")
+
+if(save_img)
+  pdf(img_name)
+par( mfrow = c(1,1), mar = c(3,3,1,0.5), mgp=c(1.5,0.5,0), bty = "l" )
+plot(0,0,  yaxt = "n",
+     xlab = "M", ylab = " ",
+     xlim = c(0,max(Mgrid) ) , ylim = c(ymin,ymax), 
+     main = paste0("Zipfs"),
+     type = "n")
+grid(lty = 1,lwd = 1, col = "gray90" )
+axis(side = 2, at = ylabs, 
+     labels = ylabs, las = 1, 
+     cex.axis = 1 )
+points( x = Mgrid, y = ub_oracle, 
+        type = "b", lwd = 1, pch = 16, lty = 2,
+        col = "black" ) 
+points( x = Mgrid, y = ub_PYP[2,], 
+        type = "b", lwd = 1, pch = 16, lty = 2, 
+        col = "darkred" ) 
+points( x = Mgrid, y = ub_FD[2,],
+        type = "b", lwd = 1, pch = 16, lty = 2,
+        col = "darkblue" )
+points( x = Mgrid, y = ub_pain[2,], 
+        type = "b", lwd = 1, pch = 16, lty = 2,
+        col = "darkorange" ) 
+## Add conf. intervals
+# segments(x0 = Mgrid, x1 = Mgrid,
+#          y0 = ub_oracle[1,], y1 = ub_oracle[3,],
+#          col = "black", lwd = 1)
+legend("bottomright",c("Oracle","PD","FDP","Freq."), 
+       lwd = 3, col = c("black","darkred","darkblue","darkorange"))
+if(save_img)
+  dev.off()
+
+# NegBin: Coverage -------------------------------------------------------------------
+Nrep_tot = ncol(oracle_mat)
+PYP_cov     = rowSums( exp(lub_Mrk_mat) > oracle_mat)/Nrep_tot
+FDP_cov     = rowSums(exp(lub_FD_mat) > oracle_mat)/Nrep_tot
+Freq_cov    = rowSums(pain_mat > oracle_mat)/Nrep_tot
+
+coverages = rbind(PYP_cov,FDP_cov,Freq_cov)
+coverages = as.data.frame(coverages)
+names(coverages) = paste0("M=",as.character(Mgrid))
+coverages
+
+file_name = paste0("../save/SimStudySpecies_coverages_NegBin_all.dat")
+save(coverages, file = file_name)
 
