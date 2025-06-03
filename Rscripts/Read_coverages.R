c9faaf56975a0400e81abe88008b3971547c3eac
Alessandro Colombi <colombi.alessandro@hotmail.it>
2025-06-03 09:57:15 +0200

    read cov file

---
+++ Rscripts/Read_coverages.R
@@ -0,0 +1,30 @@
+setwd("C:/Users/colom/bnp_upperbounds/Rscripts")
+# setwd("/home/lucia.paci/Lucia/Ale/bnp_upperbounds/Rscripts")
+
+# Librerie ----------------------------------------------------------------
+
+suppressWarnings(suppressPackageStartupMessages(library(tibble)))
+suppressWarnings(suppressPackageStartupMessages(library(parallel)))
+suppressWarnings(suppressPackageStartupMessages(library(doSNOW)))
+suppressWarnings(suppressPackageStartupMessages(library(progress)))
+suppressWarnings(suppressPackageStartupMessages(library(VGAM)))
+Rcpp::sourceCpp("../src/RcppFunctions.cpp")
+source("../R/Rfunctions.R")
+
+
+# Species - Zipfs ---------------------------------------------------------
+load("save/SimStudySpecies_coverages_zipfs_all.dat")
+round( t(coverages), 3)
+# Species - NegBin ---------------------------------------------------------
+load("save/SimStudySpecies_coverages_NegBin_all.dat")
+round( t(coverages), 3)
+# Species - Geom ---------------------------------------------------------
+load("save/SimStudySpecies_coverages_geom_all.dat")
+round( t(coverages), 3)
+# Species - Unif ---------------------------------------------------------
+load("save/SimStudySpecies_coverages_unif_all.dat")
+round( t(coverages), 3)
+# Features - Zipfs ---------------------------------------------------------
+load("save/SimStudyFeatures_coverages_zipfs_all.dat")
+round( t(coverages), 3)
+
