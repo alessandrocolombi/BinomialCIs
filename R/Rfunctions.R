8cd885cff4f5ea2bde52d329d2fcef7dbc0b701b
Alessandro Colombi <colombi.alessandro@hotmail.it>
2025-09-25 19:46:34 +0200

    implement sampling from unbounded zipfs

--- R/Rfunctions.R
+++ R/Rfunctions.R
@@ -6,4 +6,17 @@ sim_TruncatedZipfs_features = function(M,s){
 }
 
 
-
+# This function generates a zipfs distribution with parameter s.
+# Since the distribution is unbounded, it is truncated at level Mstar
+# where Mstar is so that of NOT observing any feature after the Mstar
+# is larger that 1-eps
+# P(\sum_{j=Mstar}^\infty N_j = 0) >= 1-eps
+sim_zipfs_features = function(s,n,eps){
+  if(s<=1)
+    stop("s must be positive")
+  logmstar = 1/(s-1) * ( log(n) - log(s-1) - log(-log(1-eps)) )
+  mstar = ceiling(logmstar)
+  
+  w = sim_TruncatedZipfs_features(mstar,s)
+  w
+}
