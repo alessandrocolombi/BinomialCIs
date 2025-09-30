cb537f477333c391ced951300ac716bf10749c80
Alessandro Colombi VMCatt <colombi.alessandro@hotmail.it>
2025-09-30 10:13:37 +0200

    ready to run from VMCatt

--- Rscripts/CancerData/Analysis_parallel_main.R
+++ Rscripts/CancerData/Analysis_parallel_main.R
@@ -1,9 +1,10 @@
 wd = "C:/Users/colom/BinomialCIs/Rscripts/CancerData/"
+wd = "~/Lucia/Ale/BinomialCIs/Rscripts/CancerData"
 setwd(wd)
 
 # The path to the script to execute in parallel
 scriptpath = "./Analysis_exec.R"
-SimNo <- 1:11
+SimNo <- 1:16
 
 # Parallel execution 
 idx <- SimNo[1]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
@@ -15,4 +16,9 @@ idx <- SimNo[6]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
 idx <- SimNo[7]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
 idx <- SimNo[8]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
 idx <- SimNo[9]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
-idx <- SimNo[10]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
+idx <- SimNo[11]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
+idx <- SimNo[12]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
+idx <- SimNo[13]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
+idx <- SimNo[14]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
+idx <- SimNo[15]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
+idx <- SimNo[16]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
